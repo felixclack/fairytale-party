1b0a3c5d2f90522e1f8b9f2c601101ab527cbc54
Felix Clack <felixclack@gmail.com>
2009-01-22 10:14:43 +0000

    Added titles to pages.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,31 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+default_run_options[:pty] = true
+ssh_options[:keys] = "~/.ssh/brightbox-key"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+# be sure to change these
+set :user, 'felixclack'
+set :domain, 'fairytaleparty.co.uk'
+set :application, 'fparty'
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+# the rest should be good
+set :repository,  "git@github.com:kid80/fairytale-party.git"
+set :deploy_to, "/home/#{user}/#{domain}" 
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :scm_passphrase, "saffron"
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :use_sudo, false
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+server domain, :app, :web
+role :db, domain, :primary => true
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+end
+
+task :ssh do
+  system "ssh -i ~/.ssh/brightbox-key felixclack@fairytaleparty.co.uk"
+end
